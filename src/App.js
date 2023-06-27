0431b4e17392740b63bd7ba0f0b74d7acc0f4988
Karla Florez <114744321+KarlaFlorez@users.noreply.github.com>
2023-06-27 14:34:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+<<<<<<< Updated upstream
+import React from 'react';
 
-
-import REF_ALPHA from './img/REF_ALPHA.jpg';
-import REF_POLUX_SENCILLO from './img/REF_POLUX_SENCILLO.jpg';
-import REF_ITALIA_MIXTO from './img/REF_ITALIA_MIXTO.jpg';
-
-
-
-
-
-
-function Carrusel() {
-  return (
-    <div>
-      <p> hola</p>
-      //<img src={REF_ALPHA} alt={"REF_ALPHA"}/>
-      <img src={REF_POLUX_SENCILLO} alt={"REF_POLUX_SENCILLO"}/>
-      <img src={REF_ITALIA_MIXTO} alt={"REF_ITALIA_MIXTO"}/>
-      
-    </div>
-    
-  );
-    
-  
+function App() {
+    return(
+        <div className="">
+				<div className="home-wrapper bg-img">
+					<div className="containe">
+						<div className="row">
+							<div className="t">
+								<div className="home-content">
+									<h1 className="white-text">Somos Dasaga</h1>
+									<p className="white-text">Muebles & Accesorios S.A.S.</p>
+									<a className="orange-btn" href="portafolio.pdf" target="_blank">Ir a Portafolio</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+            <div className="section-header bg-grey" id="about">
+                <h2 className="title">¿Qui&eacute;nes Somos?</h2>
+            <p className="tex">DASAGA MUEBLES &amp; ACCESORIOS S.A.S., es una empresa de servicios y manufactura, expertos en fabricaci&oacute;n, comercializaci&oacute;n, distribuci&oacute;n, mantenimiento, asesor&iacute;a
+                t&eacute;cnica, dise&ntilde;os especiales
+                e instalaci&oacute;n de mobiliario &amp; accesorios en la l&iacute;nea de oficina, comercial, e
+                institucional.
+            </p>
+            <div className="section-header">
+                <h2 className="title">Misi&oacute;n</h2>
+            </div>
+            <p className="tex">
+                Brindar un nivel profesional, &aacute;gil y eficiente a nuestros clientes,
+                caracteriz&aacute;ndonos por un
+                alto compromiso con la calidad, funcionalidad, ergonom&iacute;a y cumplimiento en nuestros
+                productos,
+                con materia prima seleccionada e insumos de primera clase.
+                Ofrecer asesor&iacute;a permanente en la fabricaci&oacute;n de sus dise&ntilde;os, satisfacer
+                plenamente las
+                necesidades de todos nuestros clientes superando sus expectativas.
+            </p>
+            <div className="section-header">
+                <h2 className="title">Visi&oacute;n</h2>
+            </div>
+            <p className="tex">
+                Ser una firma consolidada y reconocida en el mercado, por ser una empresa que provee las mejores
+                soluciones en la fabricaci&oacute;n de mobiliario, con los m&aacute;s finos acabados, bajo altos
+                est&aacute;ndares de
+                la industria, con un equipo humano, capacitado y comprometido de manera integral.
+            </p>
+        </div>
+        <div id="service" className="sectionn">
+					<div className="st">
+						<div className="section-header text-center">
+							<h2 className="title">Servicios</h2>
+						</div>
+					</div>
+					<div className="s1">
+						<div className="service">
+							<h3 className="tit">Distribuci&oacute;n</h3>
+							<p className="detail">Servicio de transporte y log&iacute;stica dentro y fuera de la ciudad.</p>
+						</div>
+						</div>
+              			<div class="s2">
+							<div class="service">
+								<h3 class="tit">Manufactura</h3>
+								<p class="detail">Fabricaci&oacute;n en madera, metal, vidrio, entre otros. Garantizando la calidad.</p>
+							</div>
+						</div>
+						<div class="s3">
+							<div class="service">
+									<h3 class="tit">Planos</h3>
+									<p class="detail">Visualice su proyecto mobiliario y la distribuci&oacute;n en ambientes 2D y
+										3D.</p>
+							</div>
+						</div>
+						<div class="s4">
+							<div class="service">
+								<h3 class="tit">Asesor&iacute;a</h3>
+								<p class="detail">Asesor&iacute;a acorde a la exigencia de nuestros clientes.</p>
+							</div>
+						</div>
+						<div class="s5">
+							<div class="service">
+								<h3 class="tit">Mantenimiento</h3>
+								<p class="detail">Consultor&iacute;a de mantenimiento preventivo y correctivo.</p>
+							</div>
+						</div>
+						<div class="s6">
+							<div class="service">
+								<h3 class="tit">Fabricaci&oacute;n</h3>
+								<p class="detail">Dise&ntilde;o, fabricaci&oacute;n e instalaci&oacute;n de todo tipo de
+										muebles.</p>
+							</div>
+						</div>
+						</div>
+        </div>
+    )
 }
 
-export default Carrusel;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+>>>>>>> Stashed changes
+export default App;
