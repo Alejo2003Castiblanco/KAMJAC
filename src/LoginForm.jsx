19dd54d605c0a8b11dec3f85fe8f0c29cd5488a4
JulianMarquez2005 <119985131+JulianMarquez2005@users.noreply.github.com>
2023-06-28 01:26:06 -0500

    formularios validados

---
+++ src/LoginForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+
+const LoginForm = () => {
+  const [email, setEmail] = useState(''); 
+
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar la lógica para enviar los datos del formulario al servidor
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // También puedes restablecer los valores de los campos después de enviar el formulario
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Correo Electrónico:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
